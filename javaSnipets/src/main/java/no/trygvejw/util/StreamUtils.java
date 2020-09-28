9ee4eedcd3f072b10ad8c109de9276d160557d5b
trygve <trygve.woldseth@gmail.com>
2020-09-28 09:22:30 +0200

    updated StreamUtils

--- javaSnipets/src/main/java/no/trygvejw/util/StreamUtils.java
+++ javaSnipets/src/main/java/no/trygvejw/util/StreamUtils.java
@@ -3,10 +3,9 @@ package no.trygvejw.util;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
 
 public class StreamUtils {
 
@@ -37,5 +36,71 @@ public class StreamUtils {
 
             outputStream.write(buffer, 0, bytesRead);
         }
+    }
+
+    // -- get numbers -- //
+    public static short readShort(BufferedInputStream inputStream) throws IOException {
+        return ByteBuffer.wrap(inputStream.readNBytes(2)).getShort();
+    }
+
+    public static int readInt(BufferedInputStream inputStream) throws IOException {
+        return ByteBuffer.wrap(inputStream.readNBytes(4)).getInt();
+    }
+
+    public static Long readLong(BufferedInputStream inputStream) throws IOException {
+        return ByteBuffer.wrap(inputStream.readNBytes(8)).getLong();
+    }
+
+    public static Float readFloat(BufferedInputStream inputStream) throws IOException {
+        return ByteBuffer.wrap(inputStream.readNBytes(4)).getFloat();
+    }
+
+    public static Double readDouble(BufferedInputStream inputStream) throws IOException {
+        return ByteBuffer.wrap(inputStream.readNBytes(4)).getDouble();
+    }
+
+    // -- write numbers -- //
+    public static void writeShort(BufferedOutputStream outputStream, short val) throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(2);
+        buffer.putShort(val);
+        outputStream.write(buffer.array());
+    }
+
+    public static void writeInt(BufferedOutputStream outputStream, int val) throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(4);
+        buffer.putInt(val);
+        outputStream.write(buffer.array());
+    }
+
+    public static void writeToStream(BufferedOutputStream outputStream, Iterator<byte[]> iterator) throws IOException {
+        while (iterator.hasNext()) {
+            outputStream.write(iterator.next());
+        }
+    }
+
+    public static void writeFileToStream(BufferedOutputStream outputStream, File file) throws IOException {
+        writeFileToStream(outputStream, file, 4096);
+    }
+
+    public static void writeFileToStream(BufferedOutputStream outputStream, File file, int bufferSize) throws IOException {
+        BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+
+
+        byte[] buffer = new byte[bufferSize];
+        int bytesRead = -1;
+
+        while ((bytesRead = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
+    }
+
+    public static void writeStringToStream(BufferedOutputStream outputStream, String string) throws IOException {
+        outputStream.write(string.getBytes());
+    }
+
+    public static void writeJsonToStream(BufferedOutputStream outputStream, JSONObject jsonObject) throws IOException {
+        writeStringToStream(outputStream, jsonObject.toString());
+
+
     }
 }
